ccf1abc1b33d81e3e34bf9969ba2a95395efe5c9
Q2P <97294453+RootQ2P@users.noreply.github.com>
2023-09-23 13:33:31 +0100

    doorlock
    doorlock

---
+++ doorlock.lua
@@ -0,0 +1,40 @@
+
+
+-- 1 created by Q2P
+Config.DoorList['1-1'] = {
+    distance = 2,
+    objName = 1052098579,
+    locked = true,
+    doorLabel = '1',
+    doorType = 'door',
+    objYaw = 0.0010702600702643,
+    fixText = false,
+    doorRate = 1.0,
+    objCoords = vec3(939.027100, -1489.573120, 30.241999),
+}
+
+-- 2 created by Q2P
+Config.DoorList['1-2'] = {
+    distance = 2,
+    objName = -2076929163,
+    locked = true,
+    doorLabel = '2',
+    doorType = 'garage',
+    objYaw = 0.0010702600702643,
+    fixText = false,
+    doorRate = 1.0,
+    objCoords = vec3(935.929077, -1489.476196, 31.161369),
+}
+
+-- 3 created by Q2P
+Config.DoorList['1-3'] = {
+    distance = 2,
+    objName = -2076929163,
+    locked = true,
+    doorLabel = '3',
+    doorType = 'garage',
+    objYaw = 0.0010702600702643,
+    fixText = false,
+    doorRate = 1.0,
+    objCoords = vec3(943.369568, -1489.476074, 31.172506),
+}
