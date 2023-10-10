1b48a27cbe70e835178fa8115a7a4475d08071d1
Q2P <97294453+RootQ2P@users.noreply.github.com>
2023-10-10 12:20:47 +0100

    Update target.lua as I fuck up lol

--- client/target.lua
+++ client/target.lua
@@ -2,27 +2,23 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local isActive = false
 local hackBlip
 
--- Thread for handling heist reset
 Citizen.CreateThread(function()
     while true do 
         Citizen.Wait(1)
         if isActive then
-            Citizen.Wait(Config.StartTimer * 60000)
+            Citizen.Wait(Config.StartTimer*60000)
             RemoveBlip(hackBlip)
             TriggerEvent('q2x:client:ResetHeist')
         end
     end
 end)
-
--- Event to reset the heist
 RegisterNetEvent('q2x:client:ResetHeist', function()
     TriggerServerEvent('q2x:server:ResetHeist')
 end)
 
--- Thread to create a ped
 CreateThread(function()
     QBCore.Functions.LoadModel(Config.Ped)
-    local created_ped = CreatePed(5, GetHashKey(Config.Ped), Config.PedCoords, false, true)
+    created_ped = CreatePed(5, GetHashKey(Config.Ped) , Config.PedCoords, false, true)
     FreezeEntityPosition(created_ped, true)
     SetEntityInvincible(created_ped, true)
     SetBlockingOfNonTemporaryEvents(created_ped, true)
