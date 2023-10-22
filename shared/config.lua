2ba16d6ce31fed029b099f0ffbbafc4a9a825e6d
Q2P <97294453+RootQ2P@users.noreply.github.com>
2023-10-22 14:27:29 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,12 +1,32 @@
-Config = {
-    warehousesmall = vector4(1094.98, -3101.78, -39.01, 78.99),
-    warehousesmallexit = vector4(1014.02, -2448.72, 28.49, 236.8),
-    warehousemedium = vector4(1071.34, -3102.2, -39.0, 78.99),
-    warehouselarge = vector4(1026.86, -3101.87, -39.0, 94.87),
-    -- DEBUG -- 
-    Debug = true,
-    Ped = "a_m_y_business_02",
-    PedCoords = vector4(699.95, -1448.85, 21.0, 303.08),
-    jobTime = 15, -- Time for the job in minutes
-    cooldown = 15, -- Cooldown in minutes
+Config = {}
+ 
+-- DEBUG -- 
+Config.Debug = true
+
+Config.Ped = "a_m_y_business_02"  
+Config.PedCoords = vector4(699.95, -1448.85, 21.0, 303.08) 
+
+Config.jobTime = 15            
+Config.cooldown = 15
+Config.BombTimer = 10 -- how long the bomb needs to explode
+Config.bomb = "weapon_stickybomb"
+Config.DoorLock = 'qb' -- use 'qb' for qb-doorlock events
+Config.DoorId = '1-1' -- name of the door in your doorlock config
+Config.DoorId2 = '1-2' -- name of the door in your doorlock config
+Config.DoorId3 = '1-3' -- name of the door in your doorlock config
+
+
+
+
+-- peds
+Config.PedGun = 'weapon_pistol' -- weapon NPC's use
+Config.SetPedAccuracy = 100 -- (between 10-100)
+Config.Shooters = {
+    ['whouse'] = {
+        locations = {
+            [1] = { 
+                peds = {vector3(935.97, -1475.74, 30.1),vector3(941.27, -1479.89, 30.1)}
+            },
+        },
+    }
 }
