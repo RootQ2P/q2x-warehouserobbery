bb18811f079f1ddfbe463c48df32a405fc21c643
Q2P <97294453+RootQ2P@users.noreply.github.com>
2023-10-22 14:26:50 +0100

    Update loot.lua

--- server/loot.lua
+++ server/loot.lua
@@ -1,49 +1,105 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('q2x:server:rewards', function()
+RegisterNetEvent('q2x:server:rewards1', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-    print("50")
+
     local random = math.random(100) 
     local random2 = math.random(20) 
     local item
 
-    if random >= 1 and random <= 9 then 
+    if random >= 1 and random <= 30 then 
         item = 'rolex'
-    elseif random >= 10 and random <= 19 then 
+    elseif random >= 30 and random <= 60 then 
         item = 'diamond_ring'
-    elseif random >= 20 and random <= 29 then 
+    elseif random >= 60 and random <= 80 then 
         item = 'goldchain'
-    elseif random >= 30 and random <= 39 then 
+    elseif random >= 80 and random <= 100 then 
         item = 'phone'
-    elseif random >= 40 and random <= 49 then 
-        item = 'weapon_advancedrifle'
-    elseif random >= 50 and random <= 59 then 
-        item = 'weapon_appistol'
-    elseif random >= 60 and random <= 69 then 
-        item = 'weapon_assaultshotgun'
-    elseif random >= 70 and random <= 79 then 
-        item = 'weapon_combatmg'
-    elseif random >= 80 and random <= 89 then 
-        item = 'weapon_combatpdw'
-    elseif random >= 90 and random <= 92 then 
+    end
+
+    if exports['qb-inventory']:AddItem(Player.PlayerData.source, item, random2, false) then
+        TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[item], 'add', random2)
+    else
+        TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'Your inventory is full already..', 'error', 2500)
+    end
+end)
+
+RegisterNetEvent('q2x:server:rewards2', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+
+    local random = math.random(100) 
+    local random2 = math.random(20) 
+
+    local item
+
+    if random >= 1 and random <= 10 then 
+        item = 'joint'
+    elseif random >= 10 and random <= 20 then 
+        item = 'crack_baggy'
+    elseif random >= 20 and random <= 30 then 
+        item = 'xtcbaggy'
+    elseif random >= 30 and random <= 40 then 
+        item = 'weed_brick'
+    elseif random >= 40 and random <= 50 then 
+        item = 'coke_brick'
+    elseif random >= 50 and random <= 60 then 
         item = 'coke_small_brick'
-    elseif random >= 93 and random <= 94 then 
+    elseif random >= 60 and random <= 70 then 
+        item = 'oxy'
+    elseif random >= 70 and random <= 80 then 
+        item = 'meth'
+    elseif random >= 80 and random <= 90 then 
+        item = 'meth'
+    elseif random >= 90 and random <= 100 then 
+        item = 'ifaks'
+    end
+
+    if exports['qb-inventory']:AddItem(Player.PlayerData.source, item, random2, false) then
+        TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[item], 'add', random2)
+    else
+        TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'Your inventory is full already..', 'error', 2500)
+    end
+end)
+
+RegisterNetEvent('q2x:server:rewards3', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+
+    local random = math.random(100) 
+    local random2 = math.random(20) 
+
+    local item
+
+    if random >= 1 and random <= 10 then 
+        item = 'weapon_advancedrifle'
+    elseif random >= 10 and random <= 20 then 
+        item = 'weapon_appistol'
+    elseif random >= 20 and random <= 30 then 
+        item = 'weapon_assaultshotgun'
+    elseif random >= 30 and random <= 40 then 
+        item = 'weapon_combatmg'
+    elseif random >= 40 and random <= 50 then 
+        item = 'weapon_combatpdw'
+    elseif random >= 50 and random <= 60 then 
+        item = 'coke_small_brick'
+    elseif random >= 60 and random <= 70 then 
         item = 'weapon_combatpistol'
-    elseif random >= 95 and random <= 96 then 
+    elseif random >= 70 and random <= 80 then 
         item = 'weapon_heavypistol'
-    elseif random >= 97 and random <= 98 then 
+    elseif random >= 80 and random <= 90 then 
         item = 'weapon_marksmanpistol'
-    elseif random >= 99 and random <= 100 then 
+    elseif random >= 90 and random <= 100 then 
         item = 'weapon_pistol'
     end
 
-    if item then
-        if exports['qb-inventory']:AddItem(Player.PlayerData.source, item, random2, false) then
-            TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[item], 'add', random2)
-        else
-            TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'Your inventory is full already..', 'error', 2500)
-        end
+    if exports['qb-inventory']:AddItem(Player.PlayerData.source, item, random2, false) then
+        TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[item], 'add', random2)
+    else
+        TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'Your inventory is full already..', 'error', 2500)
     end
 end)
